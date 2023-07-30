3c8ad358e1e947a19064e13ff663754c1df90c50
Rukky <abrukevwe@gmail.com>
2023-07-30 10:49:48 +0100

    Level one

---
+++ asset/src/components/App.js
@@ -0,0 +1,30 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Heading">
+          <h1>Let’s PL-AY!</h1>
+        </div>
+        <div className="First">
+          <h4>The Javascript Framework</h4>
+          <p>App JS should have three components. Each representing each section of the webpage</p>
+        </div>
+
+        <div className="First">
+          <h4>Responsive Design</h4>
+          <p>This entire website should be responsive down to 300px screens. It should also not overflow past 100vh and 100vw</p>
+        </div>
+
+        <div className="First">
+          <h4>Hosting</h4>
+          <p>Each level must be placed in a different branch on the github repository named React Learn</p>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
